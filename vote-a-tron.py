649dd77359b5c70a7360d80b63b61baf97a33984
Michael Still <mikal@stillhq.com>
2016-09-22 11:01:52 +1000

    Ta da!

--- vote-a-tron.py
+++ vote-a-tron.py
@@ -22,7 +22,7 @@ def get_reviews(host, query):
     return data
 
 
-def vote_on_change(host, auth, change, msg, vote, workflow):
+def vote_on_change(host, auth, change, msg, vote, workflow, dryrun=True):
     change_id = change['id']
     revision_id = change['revisions'].keys()[0]
     url = ('https://%s/a/changes/%s/revisions/%s/review'
@@ -33,22 +33,26 @@ def vote_on_change(host, auth, change, msg, vote, workflow):
 
     print('Voting : %s' % (data))
     print('On     : %s' % (change_id))
-    r = requests.post(url, auth=auth,
-                      headers={'Content-Type': 'application/json'},
-                      json=data)
-    if r.status_code == 200:
-        print('Status : OK')
+
+    if dryrun:
+        print('        : ...skipping as this is a dry run')
     else:
-        print('Status : Failed')
-        print('       : %s' % (r))
-        print('       : %s' % (r.text))
+        r = requests.post(url, auth=auth,
+                          headers={'Content-Type': 'application/json'},
+                          json=data)
+        if r.status_code == 200:
+            print('Status : OK')
+        else:
+            print('Status : Failed')
+            print('       : %s' % (r))
+            print('       : %s' % (r.text))
 
 
 def main(args):
     auth = requests.auth.HTTPDigestAuth(args.user, args.password)
     for change in get_reviews(args.host, args.query):
         vote_on_change(args.host, auth, change, args.msg, args.vote,
-                       args.workflow)
+                       args.workflow, args.bravery != 'high')
     return 0
 
 
@@ -74,6 +78,8 @@ if __name__ == '__main__':
     parser.add_argument('--host', dest='host',
                         default='review.openstack.org',
                         help=('Gerrit hostname default: %(default)s'))
+    parser.add_argument('--bravery', dest='bravery', default='low',
+                        help=('Set this to high to actually vote!'))
 
     args, extras = parser.parse_known_args()
 
